3e81bb5cdcbab6771073bbe285c798a25f0dc0ae
asiddique05 <66362879+asiddique05@users.noreply.github.com>
2021-07-09 09:31:37 +0200

    Auto clear cache (#1)

---
+++ fwb-plugin/src/main/java/sub/fwb/FWBEnvironment.java
@@ -0,0 +1,32 @@
+package sub.fwb;
+
+/**
+ * Access to environment variables.
+ *
+ */
+public class FWBEnvironment {
+	
+	public final String UNDEFINED_VALUE = "undefined";
+
+	/**
+	 * Gets an environment variable by name.
+	 */
+	private String getVariable(String name) {
+		String variable = System.getenv(name);
+		if (variable == null) {
+			System.err.println("WARNING Environment variable not set: " + name + ". Setting to '" + UNDEFINED_VALUE + "'");
+			return UNDEFINED_VALUE;
+		}
+		return variable;
+	}
+
+	public String cacheUrl() {
+		return getVariable("FWB_CACHE_URL");
+	}
+	public String fwbUser() {
+		return getVariable("FWB_USER");
+	}
+	public String fwbPassword() {
+		return getVariable("FWB_PASSWORD");
+	}
+}
--- fwb-plugin/src/main/java/sub/fwb/api/ImporterStepRunTests.java
+++ fwb-plugin/src/main/java/sub/fwb/api/ImporterStepRunTests.java
@@ -1,13 +1,24 @@
 package sub.fwb.api;
 
+import java.awt.Desktop;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Map;
 
+import org.apache.commons.codec.binary.Base64;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 import sub.ent.api.ImporterStep;
+import sub.fwb.FWBEnvironment;
 import sub.fwb.SolrTester;
+import sub.ent.backend.Environment;
 
 /**
  * Importer step that checks if the import succeeded.
@@ -18,6 +29,8 @@ public class ImporterStepRunTests extends ImporterStep {
 	/**
 	 * Gets a Solr endpoint and executes some tests on the Solr index.
 	 */
+	private static FWBEnvironment fwbenv = new FWBEnvironment();
+
 	@Override
 	public void execute(Map<String, String> params) throws Exception {
 		String solrUrl = params.get("solrUrl");
@@ -32,11 +45,56 @@ public class ImporterStepRunTests extends ImporterStep {
 			out.println();
 			out.println("WARNING in " + fail.getTestHeader() + ": " + fail.getMessage());
 		}
+		clearCache();
 	}
 
 	@Override
 	public String getStepDescription() {
 		return "Testabfragen in Solr";
+	}
+	
+	/**
+	 * Clearing the FWB API cache after finish indexing
+	 * @throws IOException
+	 */
+	private void clearCache() throws IOException {
+		try {
+			String webPage = fwbenv.cacheUrl();
+			String name = fwbenv.fwbUser();
+			String password = fwbenv.fwbPassword();
+			
+			String authString = name + ":" + password;
+			byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
+			String authStringEnc = new String(authEncBytes);
+			
+			URL url = new URL(webPage);
+			HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+			urlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
+			InputStream is = urlConnection.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			int responseCode = urlConnection.getResponseCode();
+			if (responseCode == HttpURLConnection.HTTP_OK) { // success
+				BufferedReader in = new BufferedReader(new InputStreamReader(
+						urlConnection.getInputStream()));
+				String inputLine;
+				StringBuffer response = new StringBuffer();
+
+				while ((inputLine = in.readLine()) != null) {
+					response.append(inputLine);
+				}
+				in.close();
+
+				System.out.println(response.toString());
+			} else {
+				System.out.println("GET request not worked, return server response is: '" + responseCode + "'");
+			
+			}
+			out.println("    Clearing Cache");
+
+	
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
